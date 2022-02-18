a4a300c9d9374bafb0964dbef6d6fcc18e21f15c
Etienne Boursault <43215408+EhBoss@users.noreply.github.com>
2022-02-18 23:27:17 +0100

    Déclarez des variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int epargne = 500;
+        int revenus = 2000;
+
+        // Ajout 100 à l'épargne
+        epargne += 100;
+
+        // Retrait de 50 au revenu
+        revenus -= 50;
+
+        // Mise à jour du délai d'épargne
+        int nombreDeJoursEpargne = (5000 - revenus) / 50;
+
+        // Mise à jour du revenu
+        revenus += (30-10) * 7;
+
+        // Une chaine de caractères
+        String text = "A wonderful string that consists of multiple characters";
+
+        // Un nombre à virgule
+        double percentageOfVowels = 0.0;
+
+        // Constante
+        final int NUMBEROFWEEKDAYs = 7;
+        final String MYFAVOURITEFOOD = "Ice Cream";
+
+        System.out.println(nombreDeJoursEpargne);
+        System.out.println("nombreDeJoursEpargne");
+
     }
 }
